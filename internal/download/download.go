01f6e004874d739fed98e585873f4b03900392e7
spring23 <1628420979@qq.com>
2023-08-31 13:16:12 +0800

    optimize: unnecessary close idle connections
    已经复用了本身带有连接池的client，无须每次请求后调用`CloseIdleConnections` 关闭空闲连接，以免降低效率

--- internal/download/download.go
+++ internal/download/download.go
@@ -145,7 +145,6 @@ func (r Request) Bytes() ([]byte, error) {
 		return nil, err
 	}
 	defer rd.Close()
-	defer r.client().CloseIdleConnections()
 	return io.ReadAll(rd)
 }
 
@@ -156,7 +155,6 @@ func (r Request) JSON() (gjson.Result, error) {
 		return gjson.Result{}, err
 	}
 	defer rd.Close()
-	defer r.client().CloseIdleConnections()
 
 	var sb strings.Builder
 	_, err = io.Copy(&sb, rd)
@@ -184,7 +182,6 @@ func (r Request) WriteToFile(path string) error {
 		return err
 	}
 	defer rd.Close()
-	defer r.client().CloseIdleConnections()
 	return writeToFile(rd, path)
 }
 
@@ -194,7 +191,6 @@ func (r Request) WriteToFileMultiThreading(path string, thread int) error {
 		return r.WriteToFile(path)
 	}
 
-	defer r.client().CloseIdleConnections()
 	limit := r.Limit
 	type BlockMetaData struct {
 		BeginOffset    int64
